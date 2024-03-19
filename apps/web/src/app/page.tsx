e3ae3f7c1bb64399a53610fd2d2e30e013fc8b59
Ryan Phan <ryanwithcodes@gmail.com>
2024-03-19 23:01:40 +0700

    ref(infrastructure): update config, add hover-card ui

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,11 +1,13 @@
 "use client"
 
 import React from "react"
+import { Button } from "ui//button"
 import Introduction from "../components/introduction"
 
 export default function Home(): React.ReactElement {
   return (
     <section className="h-full w-full p-4">
+      <Button>hehee</Button>
       <Introduction />
     </section>
   )
